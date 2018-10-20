6417331cb1a9a166b1ff3fd11e1475563c4e5765
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-20 14:43:34 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,11 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  hash = Hash.new(0) 
+  
+  data[:color].each do |color, names_arr|
+    names_arr.each do |name|
+      if hash[name].has_key?(:color)
+      hash[name] = {:color => }
+    end 
+  end 
 end
